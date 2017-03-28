cd56afc269c6901d4350cfffd9e2303a0e245cc3
Davic Wei <48450976@qq.com>
2017-03-28 15:19:24 +0800

    ff

--- test/src/main/java/tk/mybatis/springboot/Application.java
+++ test/src/main/java/tk/mybatis/springboot/Application.java
@@ -19,7 +19,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class Application extends WebMvcConfigurerAdapter {
 
     public static void main(String[] args) {
-    	//dfjd
+    	//dfjdy
         SpringApplication.run(Application.class, args);
     }
 
